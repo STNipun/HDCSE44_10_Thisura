07093690d11c094ccbd64333a744369aec61d3b3
STNipun <imthisuranipun@gmail.com>
2025-05-08 14:33:29 +0530

    08_08_2025 ClassRoom Activities

---
+++ 08_05_2025/index.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+// var name = "Thisura"
+
+// console.log(name)
+// var name = "Nupun"
+// console.log(name)
+
+
+// let isBool = true;
+// let number = 10;
+// let n = 10.0;
+
+// console.log(isBool)
+
+// const firstName = "Thisura";
+
+// console.log(firstName);
+
+// // firstName = 'Nipun';
+
+// console.log(firstName);
+
+// console.log(typeof(name))
+// console.log(typeof(isBool))
+
+// console.log(typeof(number))
+// console.log(typeof(n))
---
+++ 08_05_2025/objects.js
@@ -0,0 +1,19 @@
+// let person = {
+//     name : "Thisura",
+//     age : 20
+// }
+
+// console.log(person)
+
+// person.name = 'Nipun';
+
+// console.log(person.name)
+
+
+const person={}
+
+person.name = "Thisura"
+person.age = 90;
+person.height = 5.9
+
+console.log(person)
